b03971f91e5e8fce91334b1647c12245159f618b
Arshia Nayebnazar <arshianayebnazar@Arshias-MacBook-Air.local>
2020-03-15 17:07:11 -0700

    onlineDone

---
+++ src/_99_extra/Cat.java
@@ -0,0 +1,72 @@
+package _99_extra;
+
+import javax.swing.JButton;
+
+1.
+public class Cat {
+	private String name;
+	private int napsPerDay;
+	
+	
+	Cat(String name, int napsPerDay){
+		this.name = name;
+		this.napsPerDay = napsPerDay;
+	}
+	public String name() {
+		return name;
+	}
+	 public int naps() {
+		 return 50;
+	 }
+	
+
+}
+
+
+
+
+2. 
+	- JLabel
+	- JFrame
+	- JPanel
+
+
+	
+3. 
+ public String name(String x) {
+	if(x.equalsIgnoreCase("Arshia")) {
+		return "awesome";
+	}
+	else {
+		return "lame";
+	}
+}
+
+
+
+
+4.
+	- int shoes = 2;
+
+	- String iceCream = "Vanilla";
+	
+	- boolean dance = false;
+	
+	- JButton button = new JButton();
+	
+	- String cat = "Felix";
+	
+	
+	
+	
+	
+5. 
+
+public class strawberryRunner() {
+	Jam strawberry = new Jam();
+	strawberry.flavor("strawberry");
+	strawberry.spreadOnToast();
+}
+
+
+
---
+++ src/_99_extra/Checkpoint2.java
@@ -0,0 +1,58 @@
+package _99_extra;
+
+1. 
+boolean b = isInAGoodMood("Farzaneh");
+
+if(b==true) {
+	int money = askForMoney(20);
+}
+
+
+
+
+
+2.
+
+public class Pencil{
+	
+	
+	public void sharpen() {
+		System.out.println("The pencil is sharpening");
+	}
+}
+
+
+
+
+
+3.
+    ella.getFavoriteIceCream();
+	jimmy.getFavoriteIceCream();
+	pedro.getFavoriteIceCream();
+	
+	
+	
+4. 
+	MagicPower m = new MagicPower();
+	m.castSpell("fireball");
+	
+	
+	
+
+5.
+	for(int i = 0;i<11;i++) {
+		getAnswer("Math", i)
+	}
+
+6.
+	Key k = new Key("dungeon");
+	Door d = new Door();
+	boolean opened = d.openDoor(k);
+
+7.
+	Friend f = new Friend("Tony");
+	Candy c = new Candy();
+	c.getFavoriteCandy();
+	CandyShop shop = new CandyShop();
+	shop.buy(c);
+
